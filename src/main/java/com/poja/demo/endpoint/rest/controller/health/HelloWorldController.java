06fcd899288dd6c545b65c641ec62becbbb39324
nonieKley <hei.oninaina@gmail.com>
2025-06-11 22:05:48 +0300

    add bucket

--- src/main/java/com/poja/demo/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/poja/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,13 +1,18 @@
 package com.poja.demo.endpoint.rest.controller.health;
 
+import com.poja.demo.service.HelloWorldService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
+    private final HelloWorldService service;
 
     @GetMapping("/hello")
-    public String helloWorld() {
-        return "... world!";
+    public String helloWorld(@RequestParam String name) {
+        return service.uploadHelloWorldMessage(name);
     }
 }
