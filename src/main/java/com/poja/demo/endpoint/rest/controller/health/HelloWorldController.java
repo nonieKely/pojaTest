6fc16888c1744f9b85eae7b815a9f86faf770170
nonieKley <hei.oninaina@gmail.com>
2025-06-11 22:45:51 +0300

    add event

--- src/main/java/com/poja/demo/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/poja/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,8 +1,7 @@
 package com.poja.demo.endpoint.rest.controller.health;
 
-import com.poja.demo.mail.Email;
-import com.poja.demo.mail.Mailer;
-import jakarta.mail.internet.InternetAddress;
+import com.poja.demo.endpoint.event.EventProducer;
+import com.poja.demo.endpoint.event.model.SendEmailRequested;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -13,15 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-    private final Mailer mailer;
+    private final EventProducer<SendEmailRequested> eventProducer;
 
     @GetMapping("/hello")
     @SneakyThrows
     public String helloWorld(@RequestParam String to) {
-        var email =
-                new Email(new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
-
-        mailer.accept(email);
+        var event = SendEmailRequested.builder().to(to).build();
+        eventProducer.accept(List.of(event));
         return "... world!";
     }
 }
---
+++ src/main/java/com/poja/demo/service/event/SendEmailRequestedService.java
@@ -0,0 +1,25 @@
+package com.poja.demo.service.event;
+
+import com.poja.demo.endpoint.event.model.SendEmailRequested;
+import com.poja.demo.mail.Email;
+import com.poja.demo.mail.Mailer;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SendEmailRequestedService implements Consumer<SendEmailRequested> {
+    private final Mailer mailer;
+
+    @SneakyThrows
+    @Override
+    public void accept(SendEmailRequested sendEmailRequested) {
+        InternetAddress recipientAddress = new InternetAddress(sendEmailRequested.getTo());
+        mailer.accept(new Email(recipientAddress, List.of(), List.of(), "", "... world!", List.of()));
+    }
+}
+
